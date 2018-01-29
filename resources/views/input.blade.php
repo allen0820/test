dd3b3a69acdbdb813ac268acd40328ef2e03e7eb
allen0820 <s3a532035@student.ncut.edu.tw>
2018-01-29 12:13:47 +0800

    test push

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -2,10 +2,11 @@
     <p>{{$i->id}}.{{$i->input}}</p>
 @endforeach
 
-time 1114
+
 <form action="/input" method="POST">
 {{csrf_field()}}
 <input type="text" name="in">
 <input type="submit" value="送出">
+<input type="text" value="test push">
 
 </form>
